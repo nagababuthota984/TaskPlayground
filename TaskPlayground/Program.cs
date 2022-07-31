fd54e8fe34c5c6125a5f4e7693ec2c8dc6bffd2e
Thota Nagababu <nagababuthota984@outlook.com>
2022-07-31 07:48:24 +0530

    Add project files.

---
+++ TaskPlayground/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TaskPlayground;
+
+Console.WriteLine("Hello, World!");
+
+TaskPool taskPool = new TaskPool();
+//taskPool.ExecSingleTask();
+taskPool.ExecTplForLoop();
---
+++ TaskPlayground/TaskPool.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskPlayground
+{
+    public class TaskPool
+    {
+        public async void ExecSingleTask()
+        {
+            Verbose(nameof(ExecSingleTask));
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                    Console.WriteLine($"hey {i}");
+            });
+        }
+
+        public async void  ExecTplForLoop()
+        {
+            Verbose(nameof(ExecTplForLoop));
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for(int i=0;i<100;i++)
+            {
+                Console.WriteLine($"hey {i}");
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Standard for loop elapsed time - {stopwatch.Elapsed.TotalSeconds}");
+
+            stopwatch.Reset();
+            
+            stopwatch.Start();
+            Parallel.For(0,100,i =>
+            {
+                Console.WriteLine($"hey {i}");
+            });
+            stopwatch.Stop();
+            Console.WriteLine($"Parallel for loop elapsed time - {stopwatch.Elapsed.TotalSeconds}");
+            stopwatch.Reset();
+        }
+
+        public async void ExecParallelInvoke()
+        {
+            Verbose(nameof(ExecParallelInvoke));
+            Parallel.Invoke(
+                () =>
+                {
+                    Console.WriteLine("First task");
+                },
+                () =>
+                {
+                    Console.WriteLine("Second task");
+                }
+                );
+        }
+
+        public void Verbose(string methodName)
+        {
+            Console.WriteLine($"Enterring into Method  [{methodName}]");
+            Console.WriteLine("\nMethod Result\n");
+        }
+    }
+}
